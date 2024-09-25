ccf41db90db695694e51fe52ac75167f0b6446a1
siva-1317 <siva1317m@gmail.com>
2024-09-25 15:40:31 +0530

    translator

---
+++ translater.py
@@ -0,0 +1,29 @@
+import pyttsx3
+import sys
+import io
+from googletrans import Translator
+from gtts import gTTS
+import os
+
+
+engine = pyttsx3.init()
+
+voices = engine.getProperty("voices")
+engine.setProperty("voice",voices[1].id)
+
+#text to speech
+def speech(text):
+    engine.say(text)
+    engine.runAndWait()
+    
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+# Create a Translator object
+translator = Translator()
+
+# Translate English to Tamil
+result = translator.translate('Hello, how are you?', src='en', dest='ta')
+sd=result.text
+# Print the translated text
+print(sd)
+speech(sd)
